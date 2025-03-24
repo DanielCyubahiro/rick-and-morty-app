a6c5700d4ee944fe2bef495fba8ac04433938379
alissa svertokina <svertokina92@gmail.com>
2025-03-24 14:58:35 +0100

    add notes to main

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
-import {CharacterCard} from './components/CharacterCard/CharacterCard.js';
+import { CharacterCard } from "./components/CharacterCard/CharacterCard.js";
 
 const cardContainer = document.querySelector('[data-js="card-container"]');
 const searchBarContainer = document.querySelector(
-    '[data-js="search-bar-container"]');
+  '[data-js="search-bar-container"]'
+);
 
 const searchBar = document.querySelector('[data-js="search-bar"]');
 const navigation = document.querySelector('[data-js="navigation"]');
@@ -17,28 +18,37 @@ const MAX_PAGE = 42;
 const MIN_PAGE = 1;
 let currentPage = 1;
 let characters;
-const searchQuery = '';
+const searchQuery = "";
 
-nextButton.addEventListener('click', () => {
+//Pagination
+
+nextButton.addEventListener("click", () => {
   fetchCharacters(nextUrl);
-  pagination.textContent = currentPage >= MAX_PAGE
+  pagination.textContent =
+    currentPage >= MAX_PAGE
       ? `${MAX_PAGE}/{${MAX_PAGE}}`
       : `${++currentPage}/${MAX_PAGE}`;
 });
-prevButton.addEventListener('click', () => {
+prevButton.addEventListener("click", () => {
   fetchCharacters(previousUrl);
-  pagination.textContent = currentPage <= 1
+  pagination.textContent =
+    currentPage <= 1
       ? `${MIN_PAGE}/${MAX_PAGE}`
       : `${--currentPage}/${MAX_PAGE}`;
 });
-const fetchCharacters = async (URL = 'https://rickandmortyapi.com/api/character?page=1') => {
+
+//API
+const fetchCharacters = async (
+  URL = "https://rickandmortyapi.com/api/character?page=1"
+) => {
   try {
     const response = await fetch(
-        URL ?? 'https://rickandmortyapi.com/api/character?page=1');
+      URL ?? "https://rickandmortyapi.com/api/character?page=1"
+    );
     const data = await response.json();
 
     if (!response.ok) {
-      throw new Error('Failed to fetch characters');
+      throw new Error("Failed to fetch characters");
     }
 
     characters = data.results;
@@ -61,13 +71,12 @@ pagination.textContent = `${currentPage}/${MAX_PAGE}`;
 
 //Card Creation
 const rickSanchez = {
-  name: 'Rick Sanchez',
-  image: 'https://rickandmortyapi.com/api/character/avatar/1.jpeg',
-  status: 'Alive',
-  type: '',
+  name: "Rick Sanchez",
+  image: "https://rickandmortyapi.com/api/character/avatar/1.jpeg",
+  status: "Alive",
+  type: "",
   occurrences: 51,
 };
 
 const card = CharacterCard(rickSanchez);
 cardContainer.appendChild(card);
-
