d780c57c585c499ec38ca3af69200793983d999f
alissa svertokina <svertokina92@gmail.com>
2025-03-24 12:11:58 +0100

    Add changes to feature/card-creation

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-const cardContainer = document.querySelector('[data-js="card-container"]');
 const searchBarContainer = document.querySelector(
   '[data-js="search-bar-container"]'
 );
@@ -12,3 +11,18 @@ const pagination = document.querySelector('[data-js="pagination"]');
 const maxPage = 1;
 const page = 1;
 const searchQuery = "";
+
+//Card Creation
+import { CharacterCard } from "./components/CharacterCard/CharacterCard.js";
+
+const cardContainer = document.querySelector('[data-js="card-container"]');
+const rickSanchez = {
+  name: "Rick Sanchez",
+  image: "https://rickandmortyapi.com/api/character/avatar/1.jpeg",
+  status: "Alive",
+  type: "",
+  occurrences: 51,
+};
+
+const card = CharacterCard(rickSanchez);
+cardContainer.appendChild(card);
