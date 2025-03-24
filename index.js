7facdd30c8cb203cd50fff2ecf505cdd89480a2c
Daniel <cyubahirodaniel@gmail.com>
2025-03-24 12:26:43 +0100

    Add navigation

--- index.js
+++ index.js
@@ -1,14 +1,31 @@
 const cardContainer = document.querySelector('[data-js="card-container"]');
-const searchBarContainer = document.querySelector(
-  '[data-js="search-bar-container"]'
-);
+const searchBarContainer = document.querySelector('[data-js="search-bar-container"]');
 const searchBar = document.querySelector('[data-js="search-bar"]');
 const navigation = document.querySelector('[data-js="navigation"]');
-const prevButton = document.querySelector('[data-js="button-prev"]');
-const nextButton = document.querySelector('[data-js="button-next"]');
 const pagination = document.querySelector('[data-js="pagination"]');
 
 // States
+let previousUrl;
+let nextUrl;
 const maxPage = 1;
-const page = 1;
-const searchQuery = "";
+let currentPage = 1;
+
+let characters;
+const searchQuery = '';
+
+const fetchCharacters = async (URL = 'https://rickandmortyapi.com/api/character?page=1') => {
+  try {
+    const response = await fetch(
+        URL ?? 'https://rickandmortyapi.com/api/character?page=1');
+    return await response.json();
+  } catch (e) {
+    console.error(e);
+  }
+};
+
+fetchCharacters().then(data => {
+  characters = data.results;
+  nextUrl = data.info.next;
+  previousUrl = data.info.prev;
+});
+
